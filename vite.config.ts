afcce9e2c6320cc048de94b5edc0e41071f4ac6d
иван <fairsnake213@gmail.com>
2025-03-02 17:54:00 +0500

    фикс ошибок

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tg-mini-app/',
+  base: '/my-tg-mini-app/',
   plugins: [react()],
 })
